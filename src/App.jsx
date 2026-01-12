4ef627b14ba205c3b3b5c78d00880ddfc20a91c3
Mahmoud Hamed <mahmoud.hamed.shenawy@gmail.com>
2026-01-12 12:28:28 +0300

    seting up the routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,18 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./ui/Home";
+import Menu from "./features/menu/Menu";
+import Cart from "./features/cart/Cart";
+import CreateOrder from "./features/order/CreateOrder";
+import Order from "./features/order/Order";
+
 export default function App() {
-  const x = 20 ;
-  return <div>Hello vite</div>;
+  const router = createBrowserRouter([
+    { path: "/", element: <Home /> },
+    { path: "/menu", element: <Menu /> },
+    { path: "/cart", element: <Cart /> },
+    { path: "order/new", element: <CreateOrder /> },
+    { path: "order/:orderId", element: <Order /> },
+  ]);
+
+  return <RouterProvider router={router}></RouterProvider>;
 }
