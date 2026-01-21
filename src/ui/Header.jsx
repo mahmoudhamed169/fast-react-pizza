b217c6c17ff02add6ca7ee7c18ed1ffe2c955fa0
Mahmoud Hamed <mahmoud.hamed.shenawy@gmail.com>
2026-01-21 11:41:12 +0300

    setting up tailwind css v3

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -5,8 +5,8 @@ export default function Header() {
   return (
     <header>
       <Link to="/"> Fast Recat Pizza Co. </Link>
-      <Link to="/menu"> Menu </Link>
-      <Link to="/about"> About </Link>
+      {/* <Link to="/menu"> Menu </Link>
+      <Link to="/about"> About </Link> */}
 
       <SearchOrder />
     </header>
